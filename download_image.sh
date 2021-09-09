db3e9b5ae00ddae271368dd5624479c59e7077dd
suzunosuke.ito <suzunosuke.ito@optim.co.jp>
2021-09-09 18:07:46 +0900

    First commit

---
+++ download_image.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "=== リンゴの画像のダウンロード ===="
+echo "==> curl -O https://2.bp.blogspot.com/-oTqVMb3zbQ4/UgSMNNLY2wI/AAAAAAAAW-o/4nxDWnz7YsQ/s800/fruit_apple.png"
+curl -O https://2.bp.blogspot.com/-oTqVMb3zbQ4/UgSMNNLY2wI/AAAAAAAAW-o/4nxDWnz7YsQ/s800/fruit_apple.png
+echo ""
+
+echo "=== ブドウの画像のダウンロード ===="
+echo "==> curl -O https://4.bp.blogspot.com/-CQHQheuBz_o/UgSMPaqr_vI/AAAAAAAAW_Y/HQbcanw26rU/s800/fruit_grape.png"
+curl -O https://4.bp.blogspot.com/-CQHQheuBz_o/UgSMPaqr_vI/AAAAAAAAW_Y/HQbcanw26rU/s800/fruit_grape.png
+echo ""
+
+echo "=== スイカの画像のダウンロード ===="
+echo "==> curl -O curl -O https://1.bp.blogspot.com/-4twz96rvKOQ/X2VqQwHfKWI/AAAAAAABbDo/f8oN48v6xw8hGZFC230x2vm41KrymHUPgCNcBGAsYHQ/s1600/fruit_suika_kodama.png"
+curl -O https://1.bp.blogspot.com/-4twz96rvKOQ/X2VqQwHfKWI/AAAAAAABbDo/f8oN48v6xw8hGZFC230x2vm41KrymHUPgCNcBGAsYHQ/s1600/fruit_suika_kodama.png
